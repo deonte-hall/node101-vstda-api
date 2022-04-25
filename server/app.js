a695e264d382baabc830ca101ad11a0f79bf8023
Deonte H (Sasko) <deonte@Saskos-MacBook-Pro.local>
2022-04-25 11:15:48 -0700

    Initial server and file changes, server up and running, file is partially setup to return proper responses to test GET requests

--- server/app.js
+++ server/app.js
@@ -3,6 +3,47 @@ const morgan = require('morgan');
 
 const app = express();
 
+let mockData = [
+	{
+		"todoItemId": 0,
+		"name": "an item",
+		"priority": 3,
+		"completed": false
+	},
+	{
+		"todoItemId": 1,
+		"name": "another item",
+		"priority": 2,
+		"completed": false
+	},
+	{
+		"todoItemId": 2,
+		"name": "a done item",
+		"priority": 1,
+		"completed": true
+	}
+]
 // add your code here
+app.get('/', (req, res) => {
+
+    res.send({
+        status: 'ok'
+    });
+});
+
+app.get('/api/TodoItems', (req, res) => {
+
+    res.send(mockData);
+});
+
+app.get('/api/TodoItems/:number')
+
+app.get('*', (req, res)=>{
+    res.sendStatus(404);
+})
+
+
+
+
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
 const server = require('./app');
 
 // write your code here
+
+server.listen(8484, serv => {
+    console.log("server is listening on port 8484");
+});
